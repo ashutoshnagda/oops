4c03c03724b973fc45a82af10949685e629eb963
ashutoshnagda <ashutoshnagda07@gmail.com>
2025-09-06 18:02:02 +0530

    java class and object

---
+++ oop.java
@@ -0,0 +1,19 @@
+class pen {
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("Writing somthinging ");
+    }
+    public class oop{
+     public static void main(String []args){     
+     pen p1=new pen();
+     p1.color="green";
+     p1.type ="ball";
+
+      p1.write();
+      
+
+    }
+}
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,17 @@
+public class school {
+
+   private String teachersname;
+    private String principlen;
+    private int  teacherrsid;
+    private String studentsto;
+   private int totalteacher;
+
+    public String geTteachersname(){
+        return name;
+    }
+    public String seTteachersname(String name ){
+
+    }
+
+
+}
